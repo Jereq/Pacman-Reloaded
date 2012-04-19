93c845ef87cb7143e93fc990ee7b48d5a8d854b1
Sebastian Larsson <sjereq@gmail.com>
2012-04-19 13:53:24 +0200

    Continued work on Grid. Now creates vertex array.
    PACMAN-52

--- Pacman-Game/GameplayFoundations/Grid.cpp
+++ Pacman-Game/GameplayFoundations/Grid.cpp
@@ -53,15 +53,30 @@ namespace GameplayFoundations
 
 	const static Color testFile[] =
 	{
-		pacmanStart.col,	empty.col,	empty.col,	wall.col,
-		wall.col,			food.col,	food.col,	wall.col,
-		wall.col,			candy.col,	food.col,	wall.col,
-		wall.col,			food.col,	empty.col,	ghostStart.col,
+		pacmanStart.col,	empty.col,	empty.col,	wall.col,		empty.col,	empty.col,	empty.col,	food.col,	food.col,		wall.col,
+		wall.col,			food.col,	food.col,	wall.col,		empty.col,	empty.col,	empty.col,	food.col,	food.col,		wall.col,
+		wall.col,			candy.col,	food.col,	wall.col,		empty.col,	empty.col,	empty.col,	food.col,	food.col,		wall.col,
+		wall.col,			food.col,	empty.col,	ghostStart.col,	empty.col,	empty.col,	empty.col,	food.col,	food.col,		wall.col,
+		wall.col,			food.col,	food.col,	wall.col,		empty.col,	empty.col,	empty.col,	food.col,	food.col,		wall.col,
+		wall.col,			candy.col,	food.col,	wall.col,		empty.col,	empty.col,	empty.col,	food.col,	food.col,		wall.col,
+		wall.col,			food.col,	empty.col,	ghostStart.col,	empty.col,	empty.col,	empty.col,	food.col,	food.col,		wall.col,
+		wall.col,			food.col,	food.col,	wall.col,		empty.col,	empty.col,	empty.col,	food.col,	food.col,		wall.col,
+		wall.col,			candy.col,	food.col,	wall.col,		empty.col,	empty.col,	empty.col,	food.col,	ghostStart.col,	wall.col,
+		wall.col,			food.col,	empty.col,	ghostStart.col,	wall.col,	wall.col,	wall.col,	wall.col,	wall.col,		wall.col,
 	};
+	CellIndex testFileDim(10, 10);
 
-	static bool readColor(Color& _out)
+	const static Color testFileSmall[] =
 	{
-		const static size_t filesize = sizeof(testFile) / sizeof(Color);
+		empty.col,		wall.col,	pacmanStart.col,
+		wall.col,		empty.col,	food.col,
+		ghostStart.col,	candy.col,	wall.col,
+	};
+	CellIndex testFileSmallDim(3, 3);
+
+	static bool readColor(Color const* _file, Color& _out)
+	{
+		size_t filesize = (_file == testFile ? sizeof(testFile) : sizeof(testFileSmall)) / sizeof(Color);
 		static size_t curr = 0;
 
 		if (curr >= filesize)
@@ -70,7 +85,7 @@ namespace GameplayFoundations
 		}
 		else
 		{
-			_out = testFile[curr++];
+			_out = _file[curr++];
 			return true;
 		}
 	}
@@ -92,10 +107,8 @@ namespace GameplayFoundations
 	{
 		std::cout << "Filename: " << filename << std::endl;
 
-		size_t numColors = sizeof(testFile) / sizeof(Color);
-
-		size.v = 4;
-		size.u = numColors / size.v;
+		Color const* file = testFileSmall;
+		size = testFileSmallDim;
 
 		cells = new GridCell[size.u * size.v];
 
@@ -107,7 +120,7 @@ namespace GameplayFoundations
 			for (size_t cv = 0; cv < size.v; cv++)
 			{
 				Color cellC;
-				bool res = readColor(cellC);
+				bool res = readColor(file, cellC);
 				assert(res);
 
 				for (size_t i = 0; i < sizeof(colorCodes) / sizeof(ColorCode); i++)
@@ -125,6 +138,10 @@ namespace GameplayFoundations
 							startPos.u = cu;
 							startPos.v = cv;
 						}
+						else if (code.val == ghostStart.val)
+						{
+							ghostStartPos.push_back(CellIndex(cu, cv));							
+						}
 
 						break;
 					}
@@ -133,6 +150,7 @@ namespace GameplayFoundations
 		}
 
 		assert(startFound);
+		assert(ghostStartPos.size() > 0);
 
 		std::cout << "Loaded grid:" << std::endl;
 		std::cout << '+';
@@ -163,6 +181,118 @@ namespace GameplayFoundations
 		std::cout << '+' << std::endl << std::endl;
 
 		std::cout << "Pacman starts at (" << startPos.u << ", " << startPos.v << ")" << std::endl;
+
+		std::cout << "Ghosts starts at:" << std::endl;
+		for (size_t i = 0; i < ghostStartPos.size(); i++)
+		{
+			CellIndex const& pos = ghostStartPos[i];
+			std::cout << "(" << pos.u << ", " << pos.v << ")" << std::endl;
+		}
+
+		std::vector<Vertex> vertices;
+		vertices.reserve(size.u * size.v * 8);
+
+		for (size_t cu = 0; cu < size.u; cu++)
+		{
+			for (size_t cv = 0; cv < size.v; cv++)
+			{
+				if (!getCell(cu, cv).isFree())
+				{
+					continue;
+				}
+
+				const static Vec3 blockSize(1.f);
+				const static Vec3 up(0.f, 1.f, 0.f);
+
+				Vec3 basePos = Vec3((float)cu, 0.f, (float)cv) * blockSize;
+				Vec3 bPosX = basePos + Vec3(1.f, 0.f, 0.f) * blockSize;
+				Vec3 bPosZ = basePos + Vec3(0.f, 0.f, 1.f) * blockSize;
+				Vec3 bPosXZ = basePos + Vec3(1.f, 0.f, 1.f) * blockSize;
+
+				// Create Ground
+				Vertex v0 = {basePos, {0.f, 0.f}, up};
+				Vertex v1 = {bPosX, {0.f, .5f}, up};
+				Vertex v2 = {bPosZ, {.5f, 0.f}, up};
+				Vertex v3 = {bPosXZ, {.5f, .5f}, up};
+
+				vertices.push_back(v0);
+				vertices.push_back(v1);
+				vertices.push_back(v2);
+				vertices.push_back(v3);
+				vertices.push_back(v2);
+				vertices.push_back(v1);
+
+				for (size_t i = 0; i < 4; i++)
+				{
+					const static int offsetU[] = { 0, 0, -1, 1};
+					const static int offsetV[] = {-1, 1,  0, 0};
+
+					int posU = cu + offsetU[i];
+					int posV = cv + offsetV[i];
+
+					if (posU < 0 ||
+						(size_t)posU >= size.u ||
+						posV < 0 ||
+						(size_t)posV >= size.v)
+					{
+						continue;
+					}
+
+					CellIndex neighbor(posU, posV);
+
+					if (!getCell(neighbor).isFree())
+					{
+						const static Vec3 wallOffsets[4][4] =
+						{
+							{
+								Vec3(1.f, 1.f, 0.f),
+								Vec3(1.f, 0.f, 0.f),
+								Vec3(0.f, 1.f, 0.f),
+								Vec3(0.f, 0.f, 0.f),
+							},
+							{
+								Vec3(0.f, 1.f, 1.f),
+								Vec3(0.f, 0.f, 1.f),
+								Vec3(1.f, 1.f, 1.f),
+								Vec3(1.f, 0.f, 1.f),
+							},
+							{
+								Vec3(0.f, 1.f, 0.f),
+								Vec3(0.f, 0.f, 0.f),
+								Vec3(0.f, 1.f, 1.f),
+								Vec3(0.f, 0.f, 1.f),
+							},
+							{
+								Vec3(1.f, 1.f, 1.f),
+								Vec3(1.f, 0.f, 1.f),
+								Vec3(1.f, 1.f, 0.f),
+								Vec3(1.f, 0.f, 0.f),
+							}
+						};
+
+						// Create Wall
+						Vertex w0 = {basePos + wallOffsets[i][0], {.5f, 0.f}, Vec3((float)-offsetU[i], 0.f, (float)-offsetV[i])};
+						Vertex w1 = {basePos + wallOffsets[i][1], {.5f, .5f}, Vec3((float)-offsetU[i], 0.f, (float)-offsetV[i])};
+						Vertex w2 = {basePos + wallOffsets[i][2], {1.f, 0.f}, Vec3((float)-offsetU[i], 0.f, (float)-offsetV[i])};
+						Vertex w3 = {basePos + wallOffsets[i][3], {1.f, .5f}, Vec3((float)-offsetU[i], 0.f, (float)-offsetV[i])};
+
+						vertices.push_back(w0);
+						vertices.push_back(w1);
+						vertices.push_back(w2);
+						vertices.push_back(w3);
+						vertices.push_back(w2);
+						vertices.push_back(w1);
+					}
+				}
+			}
+		}
+
+		for (size_t i = 0; i < vertices.size(); i++)
+		{
+			Vertex vert = vertices[i];
+			std::cout << "Vert[" << i << "]:\t(" << vert.position.x << ", " << vert.position.y << ", " << vert.position.z << ")\t(" <<
+				vert.normal.x << ", " << vert.normal.y << ", " << vert.normal.z << ")" << std::endl;
+		}
 	}
 
 	Grid::~Grid()
--- Pacman-Game/GameplayFoundations/Grid.h
+++ Pacman-Game/GameplayFoundations/Grid.h
@@ -1,18 +1,66 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "GridCell.h"
 #include "CellIndex.h"
 
 namespace GameplayFoundations
 {
+	struct Vec3
+	{
+		union
+		{
+			float val[3];
+			struct
+			{
+				float x;
+				float y;
+				float z;
+			};
+		};
+
+		Vec3()
+			: x(0), y(0), z(0)
+		{
+		}
+
+		Vec3(float _x, float _y, float _z)
+			: x(_x), y(_y), z(_z)
+		{
+		}
+
+		Vec3(float _val)
+			: x(_val), y(_val), z(_val)
+		{
+		}
+
+		Vec3 operator*(Vec3 const& rhs) const
+		{
+			return Vec3(x * rhs.x, y * rhs.y, z * rhs.z);
+		}
+
+		Vec3 operator+(Vec3 const& rhs) const
+		{
+			return Vec3(x + rhs.x, y + rhs.y, z + rhs.z);
+		}
+	};
+
+	struct Vertex
+	{
+		Vec3 position;
+		float texPos[2];
+		Vec3 normal;
+	};
+
 	class Grid
 	{
 	private:
 		GridCell* cells;
 		CellIndex size;
 		CellIndex startPos;
+		std::vector<CellIndex> ghostStartPos;
 
 		GridCell& getCell(size_t _u, size_t _v);
 		GridCell& getCell(CellIndex _index);
